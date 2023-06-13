390803003d3022003afdfdce6ef1a6e705194ea0
Emanuelle-Oliveira <manutrindadeoliver@gmail.com>
2023-06-13 17:46:20 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import {Card, CardContent} from "@mui/material";
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Card>
+        <CardContent>
+          Olá
+        </CardContent>
+      </Card>
+    </Container>
+  );
+}
